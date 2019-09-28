af894a1de504607932089ebe26f4ee6affb7aae8
tjx <1002376198@qq.com>
2019-09-28 23:50:06 +0800

    user dao test

---
+++ src/test/java/com/tjx/MeetHere/dao/UserDaoTest.java
@@ -0,0 +1,29 @@
+package com.tjx.MeetHere.dao;
+
+import com.tjx.MeetHere.MeetHereApplication;
+import com.tjx.MeetHere.dataObject.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MeetHereApplication.class)
+class UserDaoTest {
+    @Autowired
+    private UserDao userDao;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByUserId() {
+        User user = userDao.findByUserId(1L);
+        Assertions.assertEquals("19916939173",user.getTelephone() );
+    }
+}
