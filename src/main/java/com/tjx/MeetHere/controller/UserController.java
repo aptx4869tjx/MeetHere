193c8d6d9c995225cb4ce05d32b3c5d639e9795e
tjx <1002376198@qq.com>
2019-09-28 23:48:46 +0800

    controller

---
+++ src/main/java/com/tjx/MeetHere/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.tjx.MeetHere.controller;
+
+import com.tjx.MeetHere.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired private UserDao userDao;
+    @GetMapping("/")
+    public String get(){
+        return userDao.findByUserId(1L).getTelephone();
+    }
+}
